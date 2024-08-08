2a360b733bcdb120c2cdbd9d9b67b7305d29dd31
yahya9912 <yahya4mahdi@gmail.com>
2024-08-08 22:39:08 +0300

    dawg :sob:

--- assets/data/stages/school-evil.hx
+++
@@ -1,9 +0,0 @@
-function create() {
-	importScript("data/scripts/pixel");
-	isSpooky = true;
-
-	// Make transition work between school types
-	if(PlayState.smoothTransitionData?.stage == "school") PlayState.smoothTransitionData.stage = curStage;
-
-	disableScript();
-}
